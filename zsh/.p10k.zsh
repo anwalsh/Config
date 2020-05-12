4f8ea88a3173dca7785e56c085e4627e3f68f969
Andy Walsh <andyjasonwalsh@gmail.com>
2020-05-11 20:45:05 -0500

    Modified P10K config

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -53,9 +53,9 @@
       nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
       nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
       nodeenv                 # node.js environment (https://github.com/ekalinin/nodeenv)
-      # node_version          # node.js version
-      # go_version            # go version (https://golang.org)
-      # rust_version          # rustc version (https://www.rust-lang.org)
+      node_version            # node.js version
+      go_version              # go version (https://golang.org)
+      rust_version            # rustc version (https://www.rust-lang.org)
       # dotnet_version        # .NET version (https://dotnet.microsoft.com)
       rbenv                   # ruby version from rbenv (https://github.com/rbenv/rbenv)
       rvm                     # ruby version from rvm (https://rvm.io)
