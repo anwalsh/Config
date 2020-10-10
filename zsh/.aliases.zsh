79345c51ea3167d6478c8b61612fe103f93149ef
Andy Walsh <andyjasonwalsh@gmail.com>
2020-10-10 11:43:24 -0500

    Added vs alias for `code`

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -4,32 +4,33 @@
 #   1.  MAKE TERMINAL BETTER
 #   -----------------------------
 
-alias v='nvim'                                   # Shorter Vim command because reasons
+alias v='nvim' # Shorter Vim command because reasons
+alias vs='code'
 alias ls='exa'
 alias l='exa -a'
 alias ll='exa -lgh'
 alias la='exa -lagh'
 alias lt='exa -T'
 alias lg='exa -lagh --git'
-alias cp='cp -iv'                                # Preferred 'cp' implementation
-alias mv='mv -iv'                                # Preferred 'mv' implementation
-alias mkdir='mkdir -pv'                          # Preferred 'mkdir' implementation
+alias cp='cp -iv'       # Preferred 'cp' implementation
+alias mv='mv -iv'       # Preferred 'mv' implementation
+alias mkdir='mkdir -pv' # Preferred 'mkdir' implementation
 # alias ll='ls -FGlAhp'                            # Preferred 'ls' implementation
-alias less='less -FSRXc'                         # Preferred 'less' implementation
-alias cd..='cd ../'                              # Go back 1 directory level (for fast typers)
-alias ..='cd ../'                                # Go back 1 directory level
-alias ...='cd ../../'                            # Go back 2 directory levels
-alias .3='cd ../../../'                          # Go back 3 directory levels
-alias .4='cd ../../../../'                       # Go back 4 directory levels
-alias .5='cd ../../../../../'                    # Go back 5 directory levels
-alias .6='cd ../../../../../../'                 # Go back 6 directory levels
-alias ~="cd ~"                                   # ~: Go Home
-alias c='clear'                                  # c: Clear terminal display
+alias less='less -FSRXc'         # Preferred 'less' implementation
+alias cd..='cd ../'              # Go back 1 directory level (for fast typers)
+alias ..='cd ../'                # Go back 1 directory level
+alias ...='cd ../../'            # Go back 2 directory levels
+alias .3='cd ../../../'          # Go back 3 directory levels
+alias .4='cd ../../../../'       # Go back 4 directory levels
+alias .5='cd ../../../../../'    # Go back 5 directory levels
+alias .6='cd ../../../../../../' # Go back 6 directory levels
+alias ~="cd ~"                   # ~: Go Home
+alias c='clear'                  # c: Clear terminal display
 # alias which='type -all'                          # which: Find executables
-alias path='echo -e ${PATH//:/\\n}'              # path: Echo all executable Paths
-alias show_options='shopt'                       # Show_options: display bash options settings
-alias fix_stty='stty sane'                       # fix_stty: Restore terminal settings when screwed up
-alias cic='set completion-ignore-case On'        # cic: Make tab-completion case-insensitive
+alias path='echo -e ${PATH//:/\\n}'       # path: Echo all executable Paths
+alias show_options='shopt'                # Show_options: display bash options settings
+alias fix_stty='stty sane'                # fix_stty: Restore terminal settings when screwed up
+alias cic='set completion-ignore-case On' # cic: Make tab-completion case-insensitive
 alias xclip='xclip -i'
 alias ts2date="perl -wE 'say scalar(gmtime(shift() >> 32))'"
 alias aptup="sudo apt-get update && sudo apt-get upgrade"
@@ -39,16 +40,16 @@ alias audioswap="~/GitLocal/scripts/audio-switch.sh"
 #   ------------------------------------------
 alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'' | less'
 
-alias numFiles='echo $(ls -1 | wc -l)'          # numFiles: Count of non-hidden files in current dir
-alias make1mb='mkfile 1m ./1MB.dat'             # make1mb: Creates a file of 1mb size (all zeros)
-alias make5mb='mkfile 5m ./5MB.dat'             # make5mb: Creates a file of 5mb size (all zeros)
-alias make10mb='mkfile 10m ./10MB.dat'          # make10mb: Creates a file of 10mb size (all zeros)
+alias numFiles='echo $(ls -1 | wc -l)' # numFiles: Count of non-hidden files in current dir
+alias make1mb='mkfile 1m ./1MB.dat'    # make1mb: Creates a file of 1mb size (all zeros)
+alias make5mb='mkfile 5m ./5MB.dat'    # make5mb: Creates a file of 5mb size (all zeros)
+alias make10mb='mkfile 10m ./10MB.dat' # make10mb: Creates a file of 10mb size (all zeros)
 
 #   ---------------------------
 #   2.  SEARCHING
 #   ---------------------------
 
-alias qfind="find . -name "                     # qfind: Quickly search for file
+alias qfind="find . -name " # qfind: Quickly search for file
 
 #   ---------------------------
 #   3.  PROCESS MANAGEMENT
@@ -78,22 +79,22 @@ alias ttop="top -R -F -s 10 -o rsize"
 #   3.  NETWORKING
 #   ---------------------------
 
-alias myip='wget http://ipinfo.io/ip -qO -'               # myip: Public facing IP Address
-alias netCons='lsof -i'                                   # netCons: Show all open TCP/IP sockets
-alias flushDNS='dscacheutil -flushcache'                  # flushDNS: Flush out the DNS Cache
-alias lsock='sudo /usr/sbin/lsof -i -P'                   # lsock: Display open sockets
-alias lsockU='sudo /usr/sbin/lsof -nP | grep UDP'         # lsockU: Display only open UDP sockets
-alias lsockT='sudo /usr/sbin/lsof -nP | grep TCP'         # lsockT: Display only open TCP sockets
-alias ipInfo0='ipconfig getpacket en0'                    # ipInfo0: Get info on connections for en0
-alias ipInfo1='ipconfig getpacket en1'                    # ipInfo1: Get info on connections for en1
-alias openPorts='sudo lsof -i | grep LISTEN'              # openPorts: All listening connections
-alias showBlocked='sudo ipfw list'                        # showBlocked: All ipfw rules inc/ blocked IPs
+alias myip='wget http://ipinfo.io/ip -qO -'       # myip: Public facing IP Address
+alias netCons='lsof -i'                           # netCons: Show all open TCP/IP sockets
+alias flushDNS='dscacheutil -flushcache'          # flushDNS: Flush out the DNS Cache
+alias lsock='sudo /usr/sbin/lsof -i -P'           # lsock: Display open sockets
+alias lsockU='sudo /usr/sbin/lsof -nP | grep UDP' # lsockU: Display only open UDP sockets
+alias lsockT='sudo /usr/sbin/lsof -nP | grep TCP' # lsockT: Display only open TCP sockets
+alias ipInfo0='ipconfig getpacket en0'            # ipInfo0: Get info on connections for en0
+alias ipInfo1='ipconfig getpacket en1'            # ipInfo1: Get info on connections for en1
+alias openPorts='sudo lsof -i | grep LISTEN'      # openPorts: All listening connections
+alias showBlocked='sudo ipfw list'                # showBlocked: All ipfw rules inc/ blocked IPs
 
 #   ---------------------------------------
 #   4.  SYSTEMS OPERATIONS & INFORMATION
 #   ---------------------------------------
 
-alias mountReadWrite='/sbin/mount -uw /'    # mountReadWrite:   For use when booted into single-user
+alias mountReadWrite='/sbin/mount -uw /' # mountReadWrite:   For use when booted into single-user
 
 #   ---------------------------------------
 #   5.  MISC CONFIG
