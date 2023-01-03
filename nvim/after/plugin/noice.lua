9fe68fcb1997193b80feb704b59ae3097dbbc0d1
Andy Walsh <andyjasonwalsh@gmail.com>
2023-01-03 08:37:10 -0600

    Modifications to Noice
    - Removed some aspects to fit my resolution better

--- nvim/after/plugin/noice.lua
+++ nvim/after/plugin/noice.lua
@@ -23,12 +23,6 @@ require("noice").setup({
     },
 
     views = {
-        cmdline_popup = {
-            position = {
-                row = 5,
-                col = "50%",
-            },
-        },
         popupmenu = {
             relative = "editor",
             position = "auto"
@@ -37,8 +31,6 @@ require("noice").setup({
 
     -- you can enable a preset for easier configuration
     presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = true, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = true, -- add a border to hover docs and signature help
