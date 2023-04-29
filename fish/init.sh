9daff60996e7da820ae4d21c1ef8475d3bca6a5c
Andy Walsh <andyjasonwalsh@gmail.com>
2023-04-28 21:21:22 -0500

    Added fish/init.sh and a kubectl completion plugin to fish

---
+++ fish/init.sh
@@ -0,0 +1,15 @@
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher install evanlucas/fish-kubectl-completions
+fisher install nickeb96/puffer-fish
+fisher install PatrickF1/fzf.fish
+fisher install dracula/fish
+fisher install franciscolourenco/done
+fisher install wfxr/forgit
+fisher install jorgebucaran/replay.fish
+fisher install jorgebucaran/hydro
+fisher install jorgebucaran/humantime.fish
+fisher install jorgebucaran/autopair.fish
+fisher install 0rax/fish-bd
+fisher install laughedelic/fish_logo
+fisher install jethrokuan/z
+fisher install edc/bass
