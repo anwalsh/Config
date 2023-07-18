0f92037dc9e914efb854a0b8f43c9b12919182d8
Andy Walsh <andyjasonwalsh@gmail.com>
2023-07-18 13:01:37 -0500

    Plugins updates and update to Flash config

--- nvim/lua/core/plugins/flash.lua
+++ nvim/lua/core/plugins/flash.lua
@@ -3,27 +3,30 @@ local M = {
     event = "VeryLazy",
     ---@type Flash.Config
     opts = {},
+    -- stylua: ignore
     keys = {
-        {
-            "s",
-            mode = { "n", "x", "o" },
-            function()
-                -- default options: exact mode, multi window, all directions, with a backdrop
-                require("flash").jump()
-            end,
-            desc = "Flash",
-        },
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,   desc = "Flash" },
         {
             "S",
             mode = { "n", "o", "x" },
             function() require("flash").treesitter() end,
-            desc = "Flash Treesitter",
+            desc =
+            "Flash Treesitter"
+        },
+        { "r", mode = "o",               function() require("flash").remote() end, desc = "Remote Flash" },
+        {
+            "R",
+            mode = { "o", "x" },
+            function() require("flash").treesitter_search() end,
+            desc =
+            "Treesitter Search"
         },
         {
-            "r",
-            mode = "o",
-            function() require("flash").remote() end,
-            desc = "Remote Flash",
+            "<c-s>",
+            mode = { "c" },
+            function() require("flash").toggle() end,
+            desc =
+            "Toggle Flash Search"
         },
     },
 }
