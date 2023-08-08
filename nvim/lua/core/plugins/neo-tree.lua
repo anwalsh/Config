b9d4b6aee812a16a11dfa3beaa8ee2e05f3c1492
Andy Walsh <andyjasonwalsh@gmail.com>
2023-08-08 09:24:43 -0500

    (chore)neo-tree migrations for new version/plugin updates

--- nvim/lua/core/plugins/neo-tree.lua
+++ nvim/lua/core/plugins/neo-tree.lua
@@ -24,7 +24,7 @@ local M = {
             enable_git_status = true,
             enable_diagnostics = true,
             sort_case_insensitive = false, -- used when sorting files and directories in the tree
-            sort_function = nil, -- use a custom function for sorting files and directories in the tree
+            sort_function = nil,           -- use a custom function for sorting files and directories in the tree
             event_handlers = {
                 {
                     event = "neo_tree_window_after_open",
@@ -156,9 +156,10 @@ local M = {
                     },
                 },
                 find_by_full_path_words = ".*",
-                follow_current_file = false, -- This will find and focus the file in the active buffer every
-                -- time the current file is changed while the tree is open.
-                group_empty_dirs = true, -- when true, empty folders will be grouped together
+                follow_current_file = {
+                    enabled = false,
+                },
+                group_empty_dirs = true,                -- when true, empty folders will be grouped together
                 hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
                 -- in whatever position is specified in window.position
                 -- "open_current",  -- netrw disabled, opening a directory opens within the
@@ -181,8 +182,10 @@ local M = {
                 },
             },
             buffers = {
-                follow_current_file = true, -- This will find and focus the file in the active buffer every
-                -- time the current file is changed while the tree is open.
+                follow_current_file = {
+                    enabled = true, -- This will find and focus the file in the active buffer every
+                    -- time the current file is changed while the tree is open.
+                },
                 group_empty_dirs = true, -- when true, empty folders will be grouped together
                 show_unloaded = true,
                 window = {
