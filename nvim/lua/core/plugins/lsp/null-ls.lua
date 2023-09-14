d3e47b1e56feec031cfbdaeede52564972cc9152
Andy Walsh <andyjasonwalsh@gmail.com>
2023-09-14 11:47:48 -0500

    Updates to VSCode and Nvim Configuration
    - Moving from null-ls to conform for file format.
    - VSCode adjustments for Golang environment. More to come.
    - Minor updates to plugins.
    - VSCode Diff adjustments based on new releases.

--- nvim/lua/core/plugins/lsp/null-ls.lua
+++ nvim/lua/core/plugins/lsp/null-ls.lua
@@ -1,33 +1,11 @@
 local null_ls = require("null-ls")
 local actions = null_ls.builtins.code_actions
 local diag = null_ls.builtins.diagnostics
-local fmt = null_ls.builtins.formatting
 local hover = null_ls.builtins.hover
 local cmp = null_ls.builtins.completion
 
 null_ls.setup({
     sources = {
-        -- Formatters
-        fmt.black,
-        fmt.fish_indent,
-        fmt.gofumpt,
-        -- fmt.goimports,
-        -- fmt.golines,
-        fmt.just,
-        fmt.nimpretty,
-        fmt.prettier.with({
-            args = { "--no-prose-wrap" },
-        }),
-        fmt.ruff,
-        fmt.rustfmt,
-        fmt.rustywind,
-        fmt.shfmt,
-        fmt.stylua,
-        fmt.taplo,
-        fmt.terrafmt,
-        fmt.trim_whitespace,
-        fmt.yamlfmt,
-
         -- Diagnostics
         diag.commitlint,
         diag.dotenv_linter,
