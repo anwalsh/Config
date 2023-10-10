f6a1b887f82a05e76cce289de69a90c8361f42ce
Andy Walsh <andyjasonwalsh@gmail.com>
2023-10-10 09:37:04 -0500

    Updated Indent-blankline to v3
    - Updated indent-blankline config to v3 compat

--- nvim/lua/core/plugins/indent-blankline.lua
+++ nvim/lua/core/plugins/indent-blankline.lua
@@ -1,38 +1,9 @@
 local M = {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {},
     lazy = false,
-    config = function()
-        local global = vim.api.nvim_set_var
-        local nvim_cmd = vim.api.nvim_command
-
-        nvim_cmd([[hi IndentBlanklineChar guifg=#6272a4 gui=nocombine]])
-
-        require("indent_blankline").setup({
-            show_end_of_line = true,
-            use_treesitter = true,
-            space_char_blankline = " ",
-            show_current_context = true,
-            show_current_context_start = true,
-            show_first_indent_level = true,
-            filetype_exclude = {
-                "dbout",
-                "neo-tree-popup",
-                "log",
-                "gitcommit",
-                "txt",
-                "help",
-                "NvimTree",
-                "git",
-                "flutterToolsOutline",
-                "undotree",
-                "markdown",
-                "norg",
-                "org",
-                "orgagenda",
-                "", -- for all buffers without a file type
-            },
-        })
-    end,
+    config = function() require("ibl").setup() end,
 }
 
 return M
