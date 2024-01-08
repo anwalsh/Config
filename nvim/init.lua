c7c9422e29f17ff3a805b1043b82fce263b101b4
Andy Walsh <andyjasonwalsh@gmail.com>
2024-01-08 12:19:31 -0600

    Moving Lazy validation and bootstrap to top level
    - Some light fixes to sourcing environment as well

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,81 @@
 local g, fn, opt, loop, env, cmd = vim.g, vim.fn, vim.opt, vim.loop, vim.env, vim.cmd
 
 require("aw.globals")
-require("aw.lazy")
 require("aw.mappings")
 require("aw.options")
 require("aw.util.qf")
 require("aw.autocmd")
 
+local home = env.HOME
 -- Get the OS in case I need it into the namespace
 g.os = loop.os_uname().sysname
 
 -- Same idea for variables within the system
-g.dotfiles = fn.expand("$DOTFILES_HOME") or env.dotfiles
+g.dotfiles = env.DOTFILES_HOME or fn.expand("$DOTFILES_HOME")
 g.vim_dir = g.dotfiles .. "/nvim"
-g.projects_dir = env.PROJECTS_DIR or fn.expand("GITLOCAL_HOME")
+g.projects_dir = env.GITLOCAL_HOME or fn.expand("GITLOCAL_HOME")
 g.work_dir = g.projects_dir .. "/github.com/10gen"
+
+-- Remap space as leader key
+-- Must be before lazy
+g.mapleader = " "
+g.maplocalleader = ","
+
+-- Ensure lazy.nvim plugin manager is installed
+local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not loop.fs_stat(lazypath) then
+    fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+opt.rtp:prepend(lazypath)
+
+require("lazy").setup("aw.plugins", {
+    concurrency = 50,
+    defaults = { lazy = true },
+    install = { missing = true },
+    checker = { enabled = true, notify = true, concurrency = 50, frequency = 3600 },
+    missing = true,
+    performance = {
+        cache = {
+            enabled = true,
+            -- disable_events = {},
+        },
+        rtp = {
+            disabled_plugins = {
+                "gzip",
+                "matchit",
+                "matchparen",
+                "netrw",
+                "netrwFileHandlers",
+                "netrwSettings",
+                "netrwPlugin",
+                "rplugin",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin",
+            },
+        },
+    },
+    ui = {
+        icons = {
+            cmd = "⌘",
+            config = "🛠",
+            event = "📅",
+            ft = "📂",
+            init = "⚙",
+            keys = "🗝",
+            plugin = "🔌",
+            runtime = "💻",
+            source = "📄",
+            start = "🚀",
+            task = "📌",
+        },
+    },
+})
