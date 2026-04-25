b96c9d7f07fc8f39957cc9776d058e31c86fb505
Andy Walsh <andyjasonwalsh@gmail.com>
2026-04-25 17:17:12 -0500

    Minor updates to dots

--- fish/user/functions/extract.fish
+++ fish/user/functions/extract.fish
@@ -6,28 +6,50 @@ function extract -d 'unarchive any file type'
   set -l f $argv
   if test -f $f
     switch $f
-      case '*.tar.bz2'
+      case '*.tar.bz2' '*.tbz2'
         tar xvjf $f
-      case '*.tar.gz'
+      case '*.tar.gz' '*.tgz'
         tar xvzf $f
-      case '*.bz2'
-        bunzip2 $f
-      case '*.rar'
-        unrar x $f
-      case '*.gz'
-        gunzip $f
+      case '*.tar.xz' '*.txz'
+        tar xvJf $f
+      case '*.tar.zst' '*.tzst'
+        zstd -dc $f | tar xv -
+      case '*.tar.lz4'
+        lz4 -dc $f | tar xv -
+      case '*.tar.lzo' '*.tzo'
+        lzop -dc $f | tar xv -
+      case '*.tar.lzma'
+        xz -dc --format=lzma $f | tar xv -
       case '*.tar'
         tar xvf $f
-      case '*.tbz2'
-        tar xvjf $f
-      case '*.tgz'
-        tar xvzf $f
-      case '*.zip'
+      case '*.bz2'
+        bunzip2 $f
+      case '*.gz'
+        gunzip $f
+      case '*.xz'
+        xz -d $f
+      case '*.zst'
+        zstd -d $f
+      case '*.lz4'
+        lz4 -d $f
+      case '*.lzo'
+        lzop -d $f
+      case '*.lzma'
+        xz -d --format=lzma $f
+      case '*.rar'
+        unrar x $f
+      case '*.zip' '*.war' '*.jar' '*.ear'
         unzip $f
       case '*.Z'
-        uncompress
-      case '*.7z'
+        uncompress $f
+      case '*.7z' '*.cab'
         7z x $f
+      case '*.deb'
+        ar x $f
+      case '*.rpm'
+        rpm2cpio $f | cpio -idmv
+      case '*.pkg'
+        xar -xf $f
       case '*'
         echo "'$f' cannot be extracted"
     end
